538e8dca12a6982cb93e8e8346e10a047c5cdc9b
erikanni <erik.anni@gmail.com>
2021-04-20 20:26:13 -0400

    add functioning methods and objects

--- Electronics.java
+++ Electronics.java
@@ -4,11 +4,12 @@ public class Electronics
 {
   private String event;
   private int year;
+  private String inventor;
   
-  public Electronics(String Event, int Year){
+  public Electronics(String Event, int Year, String Inventor){
     event = Event;
     year = Year;
-
+    inventor = Inventor;
   }
   
   public String getEvent(){
@@ -17,44 +18,61 @@ public class Electronics
   public int getYear(){
     return year;
   }
-  public static ArrayList<Electronics> getEvents (ArrayList<Electronics> el)
+  public String getInventor(){
+    return inventor;
+  }
+  public static ArrayList<Electronics> getEvents (ArrayList<Electronics> el, int startYear, int endYear)
     {
         int index = 0;
            while (index < el.size())
            {
                Electronics temp = el.get(index);
-               System.out.print(temp.getYear());
-             //if (el.get(index).getYear() <= 1940 || el.get(index).getYear() >= 1980)
+             if (temp.getYear() < startYear || temp.getYear() > endYear)
              {
-               //el.remove(index);
+               el.remove(index);
              }
-             //else
+             else
                index++;
            }
         return el;
     }
+    public static void printEvents (ArrayList<Electronics> el)
+    {
+        for(Electronics item:el) {
+            System.out.println(item);
+        }
+    
+    }
     public String toString(){
-    return event + " " + year;
+    return year + ": " + event + ", Invented by: " + inventor;
   }
 
   public static void main(String[] args) {
-  Electronics event1 = new Electronics("event1", 1930);
-  Electronics event2 = new Electronics("event2", 1980);
-  Electronics event3 = new Electronics("event3", 1976);
-  ArrayList<Electronics> events = new ArrayList<Electronics>();
-        events.add(event1);
-        events.add(event2);
-        events.add(event3);
-    System.out.println("Original array: ");
-   for(Electronics item:events) {
-            System.out.println(item);
-        } 
-
-  System.out.println("new array: ");
-  for(Electronics item:events) {
-            System.out.println(item);
-        }
-    getEvents(events);
+      ArrayList<Electronics> events = new ArrayList<Electronics>();
+        events.add(new Electronics("Oscillograph", 1937, "Van Ardenne, Dowling, and Bullen"));
+        events.add(new Electronics("Solar battery", 1954, "Chapin, Fuller, and Pearson"));
+        events.add(new Electronics("Transatlantic telephone cable", 1956, "UK and U.S."));
+        events.add(new Electronics("Sputnik I satellite", 1957, "Soviet Union"));
+        events.add(new Electronics("Nuclear Missile", 1957, "Kurchatov / Soviet Union"));
+        events.add(new Electronics("FORTRAN programming language", 1957, "Watson Scientific"));
+        events.add(new Electronics("First one-piece plain paper photocopier (Xerox 914)", 1959, "Xerox"));
+        events.add(new Electronics("Veroboard (Stripboard)", 1959, "Terry Fitzpatrick"));
+        events.add(new Electronics("Electronic clock", 1961, "Vogel and Cie, patented by Alexander Bain, Scottish clockmaker in 1840."));
+        events.add(new Electronics("First programmable word processor", 1972, "Automatic Electronic Systems"));
+        events.add(new Electronics("IBM Personal Computer (8088 processor)", 1981, "IBM"));
+        events.add(new Electronics("Commodore 64", 1982, "Commodore Business Machines"));
+    
+    System.out.println("Original Array: ");
+    System.out.println(" ");
+    printEvents(events); 
+    
+        int startYear = 1940;
+        int endYear = 1980;
+    getEvents(events, startYear, endYear);
+    System.out.println(" ");
+  System.out.println("New Array(Events between " + startYear +" and " + endYear + "): ");
+  System.out.println(" ");
+  printEvents(events);
 
   }
    
